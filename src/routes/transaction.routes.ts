b50066b4d6a4fc15953344efe1449e7247353f56
Thiago Pereira <thiagorpereira7@gmail.com>
2020-09-10 15:21:59 -0300

    Gerando string hash

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
+import bcrypt from 'bcryptjs';
 
 const transactionRouter = Router();
 
 transactionRouter.post('/', async (request, response) => {
   try {
     // TODO
+    const { payerIdentifier, value, recieverIdentifier } = request.body;
+
+    const data = {
+      payerIdentifier,
+      value,
+      recieverIdentifier
+    }
+
+    const qrCodeString = await bcrypt.hash(JSON.stringify(data), 8);
+    console.log('qrCodeString', qrCodeString)
+    response.json({ qrCodeString })
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
